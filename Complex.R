750426c02918ddbe771fee5ff6f70c1d547cf7ee
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-03-08 21:12:19 +0100

    Testing the max range for Integer and adding a Complex data type example

---
+++ Complex.R
@@ -0,0 +1,7 @@
+test1 <- as.complex(-25+5i)
+sqrt(test1)
+
+test2 <- complex(5,real=2,im=6)
+test2
+
+typeof(test2)
--- Integer.R
+++ Integer.R
@@ -7,3 +7,5 @@ yesInt
 is.integer(yesInt)
 
 # The maximum integer in R is 2 pow 31 ??? 1
+as.integer(2^31 - 1)
+as.integer(2^31) #it might produce a NA - NAs introduced by coercion to integer range
